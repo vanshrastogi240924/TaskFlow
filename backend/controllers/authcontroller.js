e3434e3ba05b778f66e172fa803e9a19f24ffc83
vanshrastogi240924 <vansh.rastogi.cs.2023@mitmeerut.ac.in>
2026-06-28 21:36:16 +0530

    Authentication completed

--- backend/controllers/authcontroller.js
+++ backend/controllers/authcontroller.js
@@ -1,9 +1,52 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
-
+import generateToken from "../utils/generateToken.js";
 // @desc Register User
 // @route POST /api/auth/register
 // @access Public
+// @desc Login User
+// @route POST /api/auth/login
+// @access Public
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check user exists
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "Invalid Email or Password",
+      });
+    }
+
+    // Compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Invalid Email or Password",
+      });
+    }
+
+    // Send token
+    res.status(200).json({
+      message: "Login Successful",
+      token: generateToken(user._id),
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
 
 export const registerUser = async (req, res) => {
   try {
